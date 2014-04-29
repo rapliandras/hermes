8596c19b739c887e776ce171daa015bbb8e932c4
Rápli András <rapliandras@gmail.com>
2014-04-29 22:13:16 +0200

    basic login - phase 2

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,13 +2,19 @@ class LoginController < ApplicationController
   def main
   	#do stuff here
   	if request.post? then
-  		#check for existing model and
-  		if !params[:user].nil? then 
-  			@u = params[:user]
-  		else
-  			@u = nil
+  		if params[:user][:username] && params[:user][:password] then
+  			username = params[:user][:username]
+    		password = params[:user][:password]
+    		user = find_by_username(username)
+  			#if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+  			if user && user.password == password
+  				#do nothing here
+  			else
+  				user = nil
+  			end
   		end
-  		redirect_to '/'
+
+  		#redirect_to '/'
   	end
   end
 end
